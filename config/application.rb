58991cd84063db7a421968429db427f7fed75eca
okonomi <okonomi@oknm.jp>
2022-12-03 16:46:46 +0900

    allow cloud run host

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module CloudRunJobsDbMigrateExample
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << ".a.run.app"
   end
 end
